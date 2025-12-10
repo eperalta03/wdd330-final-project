7939a124178b5ec8f178edf0994cd0cdcd2a72c1
eperalta03 <eperaltagalaviz@byupatway.edu>
2025-12-10 08:44:28 -0600

    info apod

--- scripts/main_index.js
+++ scripts/main_index.js
@@ -6,3 +6,19 @@ setupNavigation();
 setupFooter();
 loadAPOD();
 
+const url = "https://api.nasa.gov/planetary/apod?api_key=fIbBkBayfxKNssigQ5vxyemdDrKlXG9bNZ3ULnQd";
+async function apiFetch() { 
+    try{ 
+        const response= await fetch(url); 
+        if(response.ok){ 
+            const data = await response.json(); 
+            console.log(data); 
+        } else{ 
+            throw Error(await response.text()); 
+        } 
+    } catch (error){ 
+        console.log(error); } 
+} 
+apiFetch();
+
+
--- scripts/modules/display-apod.js
+++ scripts/modules/display-apod.js
@@ -6,8 +6,18 @@ export async function loadAPOD(){
 
     const div = document.getElementById("apod-container");
     const apod = document.createElement("img");
+    const info = document.createElement("div");
+
     apod.classList.add("apod");
     apod.setAttribute("src", data.url);
     apod.setAttribute("alt", data.title);
+
+    info.classList.add("info")
+    info.innerHTML= `
+        <p><strong>Title: </strong>${data.title}</p>
+        <p><strong>Image Credit & Copyright: </strong>${data.copyright}</p>
+        <p><strong>Date: </strong>${data.date}</p>
+        <p><strong>Explanation: </strong>${data.explanation}</p>`
     div.appendChild(apod);
+    div.appendChild(info);
 }
