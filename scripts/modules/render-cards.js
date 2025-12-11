399be900350a542141ccba1b9cebb24bc0141e2c
eperalta03 <eperaltagalaviz@byupatway.edu>
2025-12-11 10:18:27 -0600

    hover

--- scripts/modules/render-cards.js
+++ scripts/modules/render-cards.js
@@ -7,9 +7,9 @@ export function renderCards(data){
         const card = document.createElement("div")
         card.classList.add("card")
         card.innerHTML= `
-            <img src="${item.links[0].href}" alt="${item.data[0].title}" width= "250">
+            <img src="${item.links[0].href}" alt="${item.data[0].title}" loading="lazy">
             <div class="buttons">
-                <button class="details-btn">More details</button>
+                <button class="details-btn">View Details</button>
                 <button class="fav-btn">❤</button>
             </div>`
         cardContainer.appendChild(card);
@@ -23,13 +23,18 @@ const imgInfo= document.getElementById("img-info");
 function diplayImgInfo(img){
     imgInfo.innerHTML= "";
     imgInfo.innerHTML= `
-        <button id="close-modal">x</button>
-        <img src="${img.links[0].href}" alt="${img.data[0].title}" width= "350" height="auto">
-        <h2><strong>${img.data[0].title}</strong></h2>
-        <p><strong>NASA ID: </strong>${img.data[0].nasa_id}</p>
-        <p>${img.data[0].description_508}</p>
-        <p><strong>Date Created: </strong>${img.data[0].date_created}</p>
-        <p><strong>Center: </strong>${img.data[0].center}</p>`
+        <button id="close-modal">✖</button>
+        <div class="dialog-container">
+            <img src="${img.links[0].href}" alt="${img.data[0].title}" loading="lazy">
+            <div class="info-container">
+                <h2><strong>${img.data[0].title}</strong></h2>
+                <p><strong>NASA ID: </strong>${img.data[0].nasa_id}</p>
+                <p>${img.data[0].description}</p>
+                <p><strong>Date Created: </strong>${img.data[0].date_created}</p>
+                <p><strong>Center: </strong>${img.data[0].center}</p>
+                <button class="fav-btn">Add to Favorites</button>
+            </div>
+        </div>`
     imgInfo.showModal();
     const closeModal = document.querySelector("#close-modal");
     closeModal.addEventListener("click", () =>{
